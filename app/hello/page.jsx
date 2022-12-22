bef69a07c101358af266e9c985ffbad58412f699
Jack Howard <jack.howard@expel.io>
2022-12-22 13:21:10 -0700

    fix: Add hostname to fix prod deploy issues

--- app/hello/page.jsx
+++ app/hello/page.jsx
@@ -2,7 +2,7 @@ export default async function HelloPage() {
 	// console.log('called on server?') // note this doesn't reflect in turbo mode. yes, in `npm run dev` it does
 	// only absolute urls are allowed here
 	// I suppose because that's because this is on the server
-	const req = await fetch('http://localhost:3000/api/hello');
+	const req = await fetch(`${process.env.HOSTNAME}/api/hello`);
 	const res = await req.json();
 
 	// force a server error
